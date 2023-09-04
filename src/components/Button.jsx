c7bf95abe9ef20d58bd46955e1d7fb318f32b25b
itsrusty <82051017+itsrusty@users.noreply.github.com>
2023-09-03 19:29:10 -0700

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+const handleClick = () => {
+  window.location.href = "https://salfox-client.vercel.app/"
+};
+
 const Button = ({ styles }) => (
-  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+  <button
+    onClick={handleClick}
+    type="button"
+    className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+  >
     Comenzar
   </button>
 );
